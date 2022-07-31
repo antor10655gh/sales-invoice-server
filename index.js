a55cf707d414a598ba1cf6f33bf407fb37cb03f9
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-31 09:26:34 +0600

    add connection mongodb with node.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rma2v.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get("/", (req, res) => {
   res.send("Hello Sales Invoice!");
