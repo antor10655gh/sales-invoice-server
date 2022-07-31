782e54cc33a2c34daeaed7ef7c9912e9e64674c6
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-31 09:38:26 +0600

    successfully database connected

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rma2v.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database Connected");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello Sales Invoice!");
 });
